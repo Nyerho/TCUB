03e44a4ca039cacb262ee60294131af33c4232a0
Nyerho <2.7355326e+07+Nyerho@users.noreply.github.com>
2026-09-10 22:12:46 +0000

    Fix Vercel startup crash and template scope

--- server.js
+++ server.js
@@ -195,6 +195,7 @@ app.use((req, res, next) => {
   res.locals.error = req.session.error || null;
   res.locals.success = req.session.success || null;
   res.locals.firebaseWebConfig = firebaseWebConfig;
+  res.locals.jivoWidgetId = jivoWidgetId;
   delete req.session.error;
   delete req.session.success;
 
